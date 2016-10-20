abf10bf9f714407aa26900405f3451648c4fdb07
Alex <alex2@intelex.ca>
2016-10-20 14:45:11 +0300

    Initial commit

--- chaincode/chaincode.go
+++ chaincode/chaincode.go
@@ -1,25 +1,16 @@
 /*
-Copyright IBM Corp 2016 All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+Copyright Oleksandr Samoilov ossamoylov@gmail.com 2016 All Rights Reserved.
+This is proprietary software. You have no rights to use it unless the permission was given.
 */
 
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"strconv"
 	"time"
 )
 
@@ -27,6 +18,19 @@ import (
 type SimpleChaincode struct {
 }
 
+// declaration of Tag object
+type Tag struct {
+	Id        string `json:"id"`         //the fieldtags are needed to keep case from bouncing around
+	CreatedAt string `json:"created_at"` // creation date of tag -> when it was placed to chaincode
+	Creator   string `json:"creator"`    // creator -> who created? Obiously, Uatag
+	IssuedTo  string `json:"issued_to"`  // Company name issued to
+	IssuedAt  string `json:"issued_at"`  // the date when tag was issued to company
+}
+
+// ALL TAGS INDEXES
+var tagIndexStr = "_tagindex" //name for the key/value that will store a list of all known tags
+
+//MAIN ENTRY POINT
 func main() {
 	err := shim.Start(new(SimpleChaincode))
 	if err != nil {
@@ -35,7 +39,34 @@ func main() {
 }
 
 // Init resets all the things
-func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+func (t *SimpleChaincode) init(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+
+	var Aval int
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 1")
+	}
+
+	// Initialize the chaincode
+	Aval, err = strconv.Atoi(args[0])
+	if err != nil {
+		return nil, errors.New("Expecting integer value for asset holding")
+	}
+
+	// Write the state to the ledger
+	err = stub.PutState("abc", []byte(strconv.Itoa(Aval))) //making a test var "abc", I find it handy to read/write to it right away to test the network
+	if err != nil {
+		return nil, err
+	}
+
+	var empty []string
+	jsonAsBytes, _ := json.Marshal(empty) //marshal an emtpy array of strings to clear the index
+	err = stub.PutState(tagIndexStr, jsonAsBytes)
+	if err != nil {
+		return nil, err
+	}
+
 	// WE LOG CREATION DATE & TIME UPON INIT
 	_init_time := time.Now().String()
 	err := stub.PutState("system_created_time", []byte(_init_time))
