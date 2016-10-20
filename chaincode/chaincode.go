78e5fb5ec8308d08ec777ad21d92f227e361410d
Alex <alex2@intelex.ca>
2016-10-20 16:54:24 +0300

    Initial commit

--- chaincode/chaincode.go
+++ chaincode/chaincode.go
@@ -89,6 +89,8 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
 func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	fmt.Println("invoke is running " + function)
 
+	stub.PutState("test", []byte("1"))
+
 	// Handle different functions
 	if function == "init" {
 		//return t.Init(stub, "init", args) // init here resets all data !
