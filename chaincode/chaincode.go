023507dbc9c67f5524fae07ee06067c3019ef8b1
Alex <alex2@intelex.ca>
2016-10-21 15:12:08 +0300

    Initial commit

--- chaincode/chaincode.go
+++ chaincode/chaincode.go
@@ -21,12 +21,12 @@ type SimpleChaincode struct {
 
 // declaration of Tag object
 type Tag struct {
-	Id           *string `json:"Id"`           //the fieldtags are needed to keep case from bouncing around
-	CreatedAt    *string `json:"CreatedAt"`    // creation date of tag -> when it was physically created
-	ChaincodedAt *string `json:"ChaincodedAt"` // creation date of tag -> when it was placed to chaincode
-	Creator      *string `json:"Creator"`      // creator -> who created? Obiously, Uatag
-	IssuedTo     *string `json:"IssuedTo"`     // Company name issued to
-	IssuedAt     *string `json:"IssuedAt"`     // the date when tag was issued to company
+	Id           *string
+	CreatedAt    *string
+	ChaincodedAt *string
+	Creator      *string
+	IssuedTo     *string
+	IssuedAt     *string
 }
 
 // ALL TAGS INDEXES
